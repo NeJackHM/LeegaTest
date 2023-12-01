021ae1b8116415319110af4b15996cfbd49cd88f
nelson huaman mendez <nelson_trq@hotmail.com>
2023-11-30 22:17:00 -0300

    fiz errors

--- Example17/Interfaces/IAlbun.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Exercise17.Interfaces
-{
-    public interface IAlbun
-    {
-        void PrincipalMenuAlbun(List<Albun> albuns, List<Artista> artistas = null);
-    }
-}
--- Example17/Interfaces/IArtista.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Exercise17.Interfaces
-{
-    public interface IArtista
-    {
-        void PrincipalMenuArtista(List<Artista> artistas, List<Albun> albuns = null);
-    }
-}
